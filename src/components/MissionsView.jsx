1fcfeb80fc7c297d68647c323adf71b26c5a1059
horaciolidity <horacio.dev.sol@gmail.com>
2026-01-15 21:08:06 -0300

    validando las misiones diarias

--- src/components/MissionsView.jsx
+++ src/components/MissionsView.jsx
@@ -113,7 +113,9 @@ export function MissionsView({
 
     setVerifying(missionId);
     try {
-      const mission = MISSIONS.find(m => m.id === missionId);
+      // [FIX] Use the dynamic 'missions' prop, not the static 'MISSIONS' constant
+      // This ensures we find virtual missions (Daily Codes) that aren't in the static file
+      const mission = missions.find(m => m.id === missionId) || MISSIONS.find(m => m.id === missionId);
       let result;
 
       if (mission?.validation_type === 'daily_code') {
